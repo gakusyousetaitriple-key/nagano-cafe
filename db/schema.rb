25fc208c43ed6456046f8b33c0cda87471c0f62b
EC2 Default User <ec2-user@ip-172-31-43-178.ap-northeast-1.compute.internal>
2024-11-16 07:09:44 +0000

    Add Item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_16_063611) do
+ActiveRecord::Schema.define(version: 2024_11_16_070743) do
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
