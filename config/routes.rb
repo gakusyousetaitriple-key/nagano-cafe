3a12bbd24509c292fde0fc1a45a71eec833b082a
EC2 Default User <ec2-user@ip-172-31-37-191.ap-northeast-1.compute.internal>
2024-11-17 10:23:47 +0000

    enumdounyuu

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    resources :orders, only: [:new, :index, :show, :create]
+    get 'orders/confirm'
+    get 'orders/thanks'
+  end
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
