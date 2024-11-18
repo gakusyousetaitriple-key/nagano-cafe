a272fa58c2106530528b4cd12f4354bd5c80b029
EC2 Default User <ec2-user@ip-172-31-18-222.ap-northeast-1.compute.internal>
2024-11-18 02:38:27 +0000

    bootstrap-introduction

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/unsubscribe'
+  end
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
